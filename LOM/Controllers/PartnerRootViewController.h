e9a007e6d6454771f2558af4bd26d1be15237aa7
Torak'hehy mada <trk@iMac-de-Torakhehy.local>
2018-05-09 23:01:22 +0300

    -

---
+++ LOM/Controllers/PartnerRootViewController.h
@@ -0,0 +1,15 @@
+//
+//  PartnerRootViewController.h
+//  LOM
+//
+//  Created by Torak'hehy mada on 09/05/2018.
+//  Copyright © 2018 Kerty KAMARY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PartnerRootViewController : UIPageViewController <UIPageViewControllerDataSource> {
+    NSArray* viewControllers;
+}
+
+@end
